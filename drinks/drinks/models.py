a28f09572009362d6d840bb0982aeafab0fb8b91
Yuko <tanchho.limbu25@gmail.com>
2024-09-25 20:36:57 +0545

    first commit

---
+++ drinks/drinks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Drink(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+
+
+    def __str__(self) -> str:
+        return self.name + ', ' + self.description
